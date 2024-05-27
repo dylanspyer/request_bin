8d25f057b8a70f4ace3000828f694e8aea3f14ce
Will Conrad <w.conrad@outlook.com>
2024-05-27 16:51:11 -0500

    Initial front end with sample data
    Co-authored-by: Avery-Littlemore <Avery-Littlemore@users.noreply.github.com>

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,38 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Holds the "targeted" request on the RHS
+  const [currentRequest, setCurrentRequest] = useState(null);
+  const [requests, setRequests] = useState([
+    {
+      time: "12:00",
+      method: "POST",
+      path: "/post_request",
+      request_id: "mongoID_1",
+    },
+    {
+      time: "12:00",
+      method: "GET",
+      path: "/get_request",
+      request_id: "mongoID_2",
+    },
+  ]);
 
-  
+  // Populates LHS list
+  // Passes the MongoDB ID when clicked
+  // To be converted to a component
+  const createRequestList = () => {
+    return requests.map((r) => (
+      <button
+        key={r.request_id}
+        onClick={() => handleRequestClick(r.request_id)}
+      >
+        {" "}
+        {r.time} {r.method} {r.path}
+      </button>
+    ));
+  };
 
+  // Changes "targeted" request when a user clicks a request button on the LHS
+  const handleRequestClick = (request_id) => {
+    setCurrentRequest(request_id);
+  };
 
+  // Populates default info before a user clicks a target request
+  const defaultInfo = () => {
+    return (
+      <div>
+        <h2>Welcome to our Site</h2>
+      </div>
+    );
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Requests</h2>
+      <div>{createRequestList()}</div>
+      {currentRequest || defaultInfo()}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

4a79a77c4bf12cc55c8e230c4ae0f5ab5a511298
Avery Littlemore <avery.d.littlemore@gmail.com>
2024-05-30 18:58:57 -0400

    Added garbage collection and time on buttons

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 const baseUrl = "/api/allRequests"
 
+const deleteById = async (id) => {
+  try {
+    const response = await axios.delete(
+      `${baseUrl}/deleteOldRequest/${id}`
+    );
+    
+    return response
+    
+  } catch (error) {
+    console.error(error);
+    throw Error;
+  }
+}
+
 const getAllByToken = async (webhookToken) => {
   try {
     const response = await axios.get(
@@ -11,6 +25,17 @@ const getAllByToken = async (webhookToken) => {
     console.log("response", response);
     console.log(response.data);
     if (response.data) {
+      let index = 0
+      const currentDate = new Date()
+      while (index < response.data.length) {
+        if (currentDate - new Date(response.data[index].created_at) > 10000) {
+          await deleteById(response.data[index].request_id)
+          response.data.splice(index, 1)
+          continue
+        }
+
+        index += 1
+      }
       return response.data;
     } else {
       throw "Webhook Token Not Found";
